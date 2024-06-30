a6438ab932b053f1cc15c58428b3032faf19b4ff
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2024-06-30 11:21:40 -0300

    js

---
+++ Módulo E - Repetições em JavaScript/aula13 - while/ambiente.js
@@ -0,0 +1,13 @@
+c = 0
+
+while (c < 6) {
+    console.log(`Tudo bem? ${c}`)
+    c++
+}
+
+d = 0
+
+do {
+    console.log(`Passo ${d}`)
+    d++
+} while (d < 6)
---
+++ Módulo E - Repetições em JavaScript/aula14 - for/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+
+while (c <= 6) {
+    console.log(c)
+    c++
+}
+*/
+
+console.log('Vai começar...')
+for (var c=1; c<=6; c++) {
+    console.log(c)
+}
+console.log('FIM!')
